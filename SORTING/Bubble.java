b89b9ec5fb5895c7c42bdef7533d24b8306e2a1f
CoderArush27 <arush.ag11@gmail.com>
2023-01-14 23:16:23 +0530

    sorting

---
+++ SORTING/Bubble.java
@@ -0,0 +1,31 @@
+//it is a sorting technique in which largest element is arranged first performing a cointinous swapping
+// n*(n-1)/2 sorting is being performed
+// 
+public class Bubble {
+    public static void swap(int a,int b){
+        
+    }
+
+    public static void print(int arr[]){
+        for(int x:arr){
+            System.out.print(x+" ");
+        }
+    }
+    public static void bubbleSort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;i++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={2,1,6,7,3};
+        bubbleSort(arr);
+        print(arr);
+
+    }
+}
