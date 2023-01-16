ba555df40bee1c5c082d1cd07d863687437950ef
CoderArush27 <arush.ag11@gmail.com>
2023-01-17 00:26:13 +0530

    fibonacci

---
+++ REVISION/FibonacciNumber.java
@@ -0,0 +1,14 @@
+public class FibonacciNumber {
+    public static int fib(int n){
+        if(n<=1){
+            return n;
+        }
+        else{
+            return fib(n-1)+fib(n-2);
+        }
+    }
+    public static void main(String[] args) {
+        int n=3;
+        System.out.println(fib(9));
+    }
+}
