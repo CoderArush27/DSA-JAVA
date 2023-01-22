02d120d4e547e4ebaef7687267f8a656562e8239
CoderArush27 <arush.ag11@gmail.com>
2023-01-22 22:57:03 +0530

    Deleting cycle

--- REVISION/DeletingLoop.java
+++ REVISION/DeletingLoop.java
@@ -1,6 +1,5 @@
-// LOOP DETECTING ALGORITHM IS ALSO KNOWN AS FLOYD'S CYCLE DETECTING ALGORITHM
-public class DeletingLoop {
-    class Node{
+public class DeletingLoop{
+    public static class Node{
         int data;
         Node next;
 
@@ -9,73 +8,80 @@ public class DeletingLoop {
             this.next = null;
         }
     }
-   public static Node head;
-   public static Node tail;
-   void push(int data){
-    Node n = new Node(data);
-    if(head==null){
-        head = tail = n;
-        return;
+        public static Node head;
+        public static Node tail;
+    
+        void push(int data){
+            Node n = new Node(data);
+            if(head==null){
+                head=tail=n;
+                return;
+            }
+            n.next = head;
+            head = n;
+        }
+
+        boolean loopDetect(){
+            Node slow= head;
+            Node fast = head;
+            while(fast!=null && fast.next!=null){
+                slow = slow.next;
+                fast = fast.next.next;
+        
+                if(fast==slow)
+                    return true; 
+            }
+            return false;
+        }
+
+        void deleteLoop(){
+            Node slow = head;
+            Node fast = head;
+            boolean flag=false;
+            while(fast!=null && fast.next!=null){
+                slow = slow.next;
+                fast = fast.next.next;
+
+                if(slow==fast){
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag==false)
+                return;
+            
+            slow = head;
+            Node prev = null;
+            while(slow!=fast){
+                prev = fast;
+                slow = slow.next;
+                fast = fast.next;
+            }
+            prev.next = null;
+        }
+
+
+    public static void main(String[] args) {
+        DeletingLoop d = new DeletingLoop();
+        d.push(10);
+        d.push(20);
+        d.push(30);
+        d.push(40);
+        d.push(50);
+        d.push(60);
+        Node temp = d.head.next.next.next;
+        d.head.next.next.next.next.next.next = temp;
+        if(d.loopDetect())
+            System.out.println("LOOP DETECTED");
+        else
+            System.out.println("NOT DETECTED");
+
+        d.deleteLoop();
+
+        if(d.loopDetect())
+            System.out.println("LOOP DETECTED");
+        else
+            System.out.println("NOT DETECTED");
+
     }
-    n.next = head;
-    head = n;
-   }
-
-   int length(Node h){
-    int count=0;
-    Node temp=h;
-    while(temp!=null){
-        count++;
-        temp=temp.next;
-    }
-    return count;
-}
-
-boolean loopDetect(Node h){
-    Node slow= h;
-    Node fast = h;
-    while(fast!=null && fast.next!=null){
-        slow = slow.next;
-        fast = fast.next.next;
-
-        if(fast==slow)
-            return true; 
-    }
-    return false;
-}
-void deleteLoop(Node h){
-    Node slow= h;
-    Node fast = h;
-    Node prev = null;
-    while(fast!=null && fast.next!=null){
-        slow = slow.next;
-        fast = fast.next.next;
-
-        if(fast==slow)
-           slow = head;
-           break;
-    }
-    while(slow!=fast){
-       prev = fast;
-       slow = slow.next;
-       fast = fast.next;
-    }
-    prev.next = null;
-}
-public static void main(String[] args) { 
-    DeletingLoop l = new DeletingLoop();
-    l.push(10);
-    l.push(20);
-    l.push(30);
-    l.push(40);
-    l.push(50);
-    l.push(60);
-    l.head.next.next.next.next = l.head;
-
-    if(l.loopDetect(null))
-        System.out.println("LOOP DETECTED");
-    else    
-        System.out.println("LOOP NOT DETECTED");
-
-}
 }
