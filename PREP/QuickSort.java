320295d8b4d88ee5d8708e1dd104e6c3e95a7b0b
CoderArush27 <arush.ag11@gmail.com>
2023-03-26 15:46:17 +0530

    Quick sort

---
+++ PREP/QuickSort.java
@@ -0,0 +1,42 @@
+public class QuickSort {
+    public void quickSorting(int arr[],int si,int ei){
+        if(si>=ei)
+            return;
+        
+        int pIdx = partition(arr,si,ei);
+        quickSorting(arr, si, pIdx-1);
+        quickSorting(arr, pIdx+1, ei);
+    }
+
+    public int partition(int arr[],int si,int ei){
+        int j=si-1;
+        int pivot = arr[ei];
+        for(int i=si;i<ei;i++){
+            if(arr[i]<=pivot){
+                j++;
+                int temp = arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        j++;
+        int temp = pivot;
+        arr[ei] = arr[j];
+        arr[j] = temp;
+
+        return j;
+    }
+
+    public void print(int a[]){
+        for(int x:a){
+            System.out.print(x+" ");
+        }
+       }
+    public static void main(String[] args) {
+        QuickSort q = new QuickSort();
+        int arr[]={6,3,9,8,2,5};
+        int n = arr.length;
+        q.quickSorting(arr, 0,n-1);
+        q.print(arr);
+    }
+}
