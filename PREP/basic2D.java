866ca722d0a921e50c7a66367e444b7c209e6321
CoderArush27 <arush.ag11@gmail.com>
2023-03-29 00:47:52 +0530

    2d array

---
+++ PREP/basic2D.java
@@ -0,0 +1,7 @@
+public class basic2D {
+    public static void main(String[] args) {
+        int arr[][]={{1,3},{2,6},{8,10},{15,18}};
+        System.out.println("COLUMN LENGTH "+arr[0].length);
+        System.out.println("ROW LENGTH "+arr.length);
+    }
+}
