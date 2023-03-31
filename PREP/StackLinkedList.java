0b75674f12e1654c1d1b31dacf0a05f0be82218b
CoderArush27 <arush.ag11@gmail.com>
2023-03-31 13:51:53 +0530

    stacks

---
+++ PREP/StackLinkedList.java
@@ -0,0 +1,58 @@
+public class StackLinkedList {
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data= data;
+            this.next = null;
+        }
+    }
+    static class Stack{
+        static Node head = null;
+
+        public static boolean isEmpty(){
+            if(head==null)
+                return true;
+
+            return false;
+        }
+
+        public static void push(int data){
+            Node n = new Node(data);
+            if(isEmpty()){
+            head = n;
+            return;
+            }
+            n.next = head;
+            head = n;
+        }
+
+        public static int pop(){
+            if(isEmpty())
+             return -1;
+            
+             int top = head.data;
+             head = head.next;
+             return top;
+        }
+        public static int peek(){
+            if(isEmpty())
+                return -1;
+            
+            return head.data;
+        }
+    }
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.push(40);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
