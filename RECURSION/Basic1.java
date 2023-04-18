69ffde72c0854247964814ea0c17c3997e684352
CoderArush27 <arush.ag11@gmail.com>
2023-04-18 17:00:03 +0530

    basic statements

---
+++ RECURSION/Basic1.java
@@ -0,0 +1,14 @@
+public class Basic1 {
+    public static void main(String[] args) {
+        int a = 5;
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<=a;j++){
+                if(j==2)
+                return;
+                System.out.print(j+" ");
+                
+            }
+            System.out.println();
+        }
+    }
+}
